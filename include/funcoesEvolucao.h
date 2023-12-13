cc91ab5dbe4743e902e18d330f90b78a6f66a1c3
rhayna <rhaynacasado@usp.br>
2023-12-13 00:44:18 +0000

    mutacao header

--- include/funcoesEvolucao.h
+++ include/funcoesEvolucao.h
@@ -12,6 +12,8 @@ void exterminio(individuo *ind);
 void elitismo(individuo *ind, int *maxfit1, int *maxfit2);
 individuo misturaGene(individuo indini, individuo indfim);
 void crossover(individuo *ind, int maxfit1, int maxfit2);
+void mutacaoQuatro(individuo *novaPop);
+void mutacao(individuo *novaPop);
 void cruzamento(individuo *ind);
 
 #endif
