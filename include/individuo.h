8914ffabd050823bfd7a465a308ce752b116832c
Ana Araujo <anaaraujoz@outlook.com>
2023-12-12 22:29:50 -0300

    Update individuo.h

--- include/individuo.h
+++ include/individuo.h
@@ -11,7 +11,8 @@ typedef struct{
     float pontos;
     int posi;
     int posj;
-    int caminho[TAM];
+    int caminho[TAM]; // sequencia de movimentos do individuo, dado pela numero da direçao para qual se moveu
+    // 0/4: p cima, 1/5: p baixo, 2/6: esquerda, 3/7: direita
 } individuo;
 
 int moveIndividuoInicial(int labirinto[LINHAS][COLUNAS], individuo *ind, FILE *arquivo);
